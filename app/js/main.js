a794ef1beda927e832d6541a3e8f919cccf7993f
unfelix <shevko.evheniy@gmail.com>
2020-10-28 15:41:43 +0200

    add slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,10 @@
 $(function(){
 
 $('.slider').slick({
-  infinite: true
+  infinite: true,
+  dots: true,
+  nextArrow: '<img class="slider-arrows slider-arrows__right" src="./img/next-arrow.png" alt="">',
+  prevArrow: '<img class="slider-arrows slider-arrows__left" src="./img/prev-arrow.png" alt="">'
 })
-
   
 });
